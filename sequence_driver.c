981b326ada312dbfce4d695f391aae53867cf159
Pedro Cruz <pedrocruz657@gmail.com>
2017-08-15 23:59:31 -0600

    Sequence driver module added

---
+++ sequence_driver.c
@@ -0,0 +1,249 @@
+/*
+ *  sequence_driver.c - Create an input/output character device
+ */
+
+#include <linux/kernel.h>	/* We're doing kernel work */
+#include <linux/module.h>	/* Specifically, a module */
+#include <linux/fs.h>
+#include <asm/uaccess.h>	/* for get_user and put_user */
+
+#include "sequence_driver.h"
+#define SUCCESS 0
+#define DEVICE_NAME "char_dev"
+#define BUF_LEN 80
+
+/* 
+ * Is the device open right now? Used to prevent
+ * concurent access into the same device 
+ */
+static int Device_Open = 0;
+
+/* 
+ * The message the device will give when asked 
+ */
+
+static int number = 1;
+static int *number_ptr;
+
+/* 
+ * How far did the process reading the message get?
+ * Useful if the message is larger than the size of the
+ * buffer we get to fill in device_read. 
+ */
+
+/* 
+ * This is called whenever a process attempts to open the device file 
+ */
+static int device_open(struct inode *inode, struct file *file)
+{
+#ifdef DEBUG
+	printk(KERN_INFO "device_open(%p)\n", file);
+#endif
+
+	/* 
+	 * We don't want to talk to two processes at the same time 
+	 */
+	if (Device_Open)
+		return -EBUSY;
+
+	Device_Open++;
+	/*
+	 * Initialize the message 
+	 */
+		
+
+	
+
+	try_module_get(THIS_MODULE);
+	return SUCCESS;
+}
+
+static int device_release(struct inode *inode, struct file *file)
+{
+#ifdef DEBUG
+	printk(KERN_INFO "device_release(%p,%p)\n", inode, file);
+#endif
+
+	/* 
+	 * We're now ready for our next caller 
+	 */
+	Device_Open--;
+
+	module_put(THIS_MODULE);
+	return SUCCESS;
+}
+
+/* 
+ * This function is called whenever a process which has already opened the
+ * device file attempts to read from it.
+ */
+static ssize_t device_read(struct file *file, int __user * buffer)
+{
+
+
+
+	
+		
+	/*if(copy_to_user(buffer, seq_ptr, sizeof(sequence))){
+		
+		return -EACCES;
+	}*/
+
+	put_user(*(number_ptr), buffer);
+	number++;
+	number_ptr = &number;
+	//put_user(*(Message_Ptr++), buffer++);
+	
+
+#ifdef DEBUG
+	printk(KERN_INFO "device_read(%p,%p,%d)\n", file);
+#endif
+
+	
+
+
+	return sizeof(sequence);
+}
+
+
+/* 
+ * This function is called when somebody tries to
+ * write into our device file. 
+ */
+static ssize_t device_write(struct file *file,
+	     const int __user * buffer, size_t length, loff_t * offset)
+{
+	
+
+#ifdef DEBUG
+	printk(KERN_INFO "device_write(%p,%s,%d)", file, buffer, length);
+#endif
+
+	
+	
+
+	
+	
+
+	/* 
+	 * Again, return the number of input characters used 
+	 */
+	return 0;
+}
+
+/* 
+ * This function is called whenever a process tries to do an ioctl on our
+ * device file. We get two extra parameters (additional to the inode and file
+ * structures, which all device functions get): the number of the ioctl called
+ * and the parameter given to the ioctl function.
+ *
+ * If the ioctl is write or read/write (meaning output is returned to the
+ * calling process), the ioctl call returns the output of this function.
+ *
+ */
+long  device_ioctl(	/* see include/linux/fs.h */
+		 struct file *file,	/* ditto */
+		 unsigned int ioctl_num,	/* number and param for ioctl */
+		 unsigned long ioctl_param)
+{
+	//long ioctl(struct file *filp,unsigned int cmd, unsigned long arg)
+
+
+	/* 
+	 * Switch according to the ioctl called 
+	 */
+	
+	switch (ioctl_num) {
+	case IOCTL_SET_MSG:
+		
+		//seq.value = 78;
+		//seq_ptr = &seq;
+		device_write(file, (int *)ioctl_param, 0, 0);
+		break;
+
+	case IOCTL_GET_MSG:
+		/* 
+		 * Give the current message to the calling process - 
+		 * the parameter we got is a pointer, fill it. 
+		 */
+		
+		   
+		put_user(number, (int *)ioctl_param);
+		number++;
+		//number_ptr = &number;
+		
+		
+		break;
+
+	case IOCTL_GET_NTH_BYTE:
+		/* 
+		 * This ioctl is both input (ioctl_param) and 
+		 * output (the return value of this function) 
+		 */
+		return 1;
+		break;
+	}
+
+	return SUCCESS;
+}
+
+/* Module Declarations */
+
+/* 
+ * This structure will hold the functions to be called
+ * when a process does something to the device we
+ * created. Since a pointer to this structure is kept in
+ * the devices table, it can't be local to
+ * init_module. NULL is for unimplemented functions. 
+ */
+struct file_operations Fops = {
+	.read = device_read,
+	.write = device_write,
+	.unlocked_ioctl = device_ioctl,
+	.open = device_open,
+	.release = device_release,	/* a.k.a. close */
+};
+
+/* 
+ * Initialize the module - Register the character device 
+ */
+int init_module()
+{
+	int ret_val;
+	/* 
+	 * Register the character device (atleast try) 
+	 */
+	ret_val = register_chrdev(MAJOR_NUM, DEVICE_NAME, &Fops);
+
+	/* 
+	 * Negative values signify an error 
+	 */
+	if (ret_val < 0) {
+		printk(KERN_ALERT "%s failed with %d\n",
+		       "Sorry, registering the character device ", ret_val);
+		return ret_val;
+	}
+
+	printk(KERN_INFO "%s The major device number is %d.\n",
+	       "Registeration is a success", MAJOR_NUM);
+	printk(KERN_INFO "If you want to talk to the device driver,\n");
+	printk(KERN_INFO "you'll have to create a device file. \n");
+	printk(KERN_INFO "We suggest you use:\n");
+	printk(KERN_INFO "mknod %s c %d 0\n", DEVICE_FILE_NAME, MAJOR_NUM);
+	printk(KERN_INFO "The device file name is important, because\n");
+	printk(KERN_INFO "the ioctl program assumes that's the\n");
+	printk(KERN_INFO "file you'll use.\n");
+
+	return 0;
+}
+
+/* 
+ * Cleanup - unregister the appropriate file from /proc 
+ */
+void cleanup_module()
+{
+
+	unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
+
+
+}
