b58faebf822d1ecd17b66981c0a52b127f833a6c
Pedro Cruz <pedrocruz657@gmail.com>
2017-09-10 20:27:19 -0600

    insmod script updated

--- modules/mysql/insmod.sh
+++ modules/mysql/insmod.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 insmod mysql_sequence_driver.ko
+insmod mysql_sequence_handler.ko
 mknod /dev/mysql_seq_dev c 100 0
 mknod /dev/mysql_seq_handler c 101 0
-insmod mysql_sequence_handler.ko
-grep sequences /proc/kallsyms 
+
+
