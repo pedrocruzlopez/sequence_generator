5108d30b3dd541e26714cc36b1f7d92b7ce1f5ef
Pedro Cruz <pedrocruz657@gmail.com>
2017-09-28 23:08:07 -0600

    Add validations to add mknods

--- modules/mysql/insmod.sh
+++ modules/mysql/insmod.sh
@@ -3,8 +3,16 @@ echo "Installing modules..."
 insmod mysql_sequence_driver.ko
 insmod mysql_sequence_handler.ko
 echo "Creating devices files..."
-mknod /dev/mysql_seq_dev c 100 0
-mknod /dev/mysql_seq_handler c 101 0
+
+
+if [ -e /dev/mysql_seq_dev ]
+then
+    echo "devices already created"
+else
+   	mknod /dev/mysql_seq_dev c 100 0
+	mknod /dev/mysql_seq_handler c 101 0
+fi
+
 echo "Success!"
 
 
